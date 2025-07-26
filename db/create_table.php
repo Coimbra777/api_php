df34646ccd6cef116ba479cab0cb7351c626fb6d
Gabrieel Coimbra <gabrielfelipe.coimbra@gmail.com>
2025-07-26 14:07:52 -0300

    feat: adição endpoint para inserir dados

--- db/create_table.php
+++ db/create_table.php
@@ -4,13 +4,13 @@ require_once 'db.class.php';
 
 $pdo = DB::connect();
 
-$sql = "CREATE TABLE IF NOT EXISTS usuarios (
+$sql = "CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     email VARCHAR(100),
     cidade VARCHAR(100),
     estado VARCHAR(100)
-)";
+);";
 
 $pdo->exec($sql);
 
